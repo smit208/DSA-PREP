8e365a6861832e5682e8c0f064a373b65bb868a4
smit208 <smitvp2005@gmail.com>
2026-03-25 08:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.15%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -7,7 +7,7 @@ public:
             int mid=l+(r-l)/2;
             if(target==nums[mid])  return mid;
             else if(target<nums[mid])  r=mid-1;
-            else if(target>nums[mid])  l=mid+1;
+            else l=mid+1;
         }
         return -1;
     }
