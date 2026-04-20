9369706ec3bf7ac10a80096b2e23c01ecc63b9da
smit208 <smitvp2005@gmail.com>
2026-04-20 16:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (38.79%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> mp;
+        int count=0;
+        for(char ch:s){
+           
+                mp[ch]++;
+            
+        }
+        bool hasOdd=false;
+        for(auto it:mp){
+            int freq=it.second;
+            if(freq%2==0){
+                count+=freq;
+            }
+            else{
+                count+=freq-1;
+                hasOdd=true;   
+            }
+
+        }
+        if(hasOdd)  count+=1;
+        return count;
+    }
+};
